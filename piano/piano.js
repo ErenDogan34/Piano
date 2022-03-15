18786ac4c854efec80c92f7a78649795ae53cfda
Eren Doğan <101631700+ErenDogan34@users.noreply.github.com>
2022-03-15 12:40:49 +0300

    Add files via upload

---
+++ piano/piano.js
@@ -0,0 +1,37 @@
+var one = new Audio();
+var two = new Audio();
+var three = new Audio();
+var four = new Audio();
+var five = new Audio();
+var six = new Audio();
+var seven = new Audio();
+var eight = new Audio();
+var nine = new Audio();
+var ten = new Audio();
+var bir = new Audio();
+var iki = new Audio();
+var uc = new Audio();
+var dort = new Audio();
+var bes = new Audio();
+var alti = new Audio();
+var yedi = new Audio();
+
+
+one.src="tune/68448__pinkyfinger__piano-g.wav"
+two.src="tune/68447__pinkyfinger__piano-g.wav"
+three.src="tune/68446__pinkyfinger__piano-f.wav"
+four.src="tune/68445__pinkyfinger__piano-f.wav"
+five.src="tune/68444__pinkyfinger__piano-eb.wav"
+six.src="tune/68443__pinkyfinger__piano-e.wav"
+seven.src="tune/68442__pinkyfinger__piano-d.wav"
+eight.src="tune/68441__pinkyfinger__piano-c.wav"
+nine.src="tune/68440__pinkyfinger__piano-c.wav"
+ten.src="tune/68439__pinkyfinger__piano-bb.wav"
+
+bir.src="tune/piano_C_sharp.mp3"
+iki.src="tune/piano_D.mp3"
+uc.src="tune/piano_D_sharp.mp3"
+dort.src="tune/piano_E.mp3"
+bes.src="tune/piano_F.mp3"
+alti.src="tune/piano_F_sharp.mp3"
+yedi.src="tune/piano_G.mp3"
